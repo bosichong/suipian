358d43fbaf8ed8a173e357b2638d2d130646f757
J.sky <bosichong@qq.com>
2024-10-16 10:04:44 +0800

    更新壁纸

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,7 @@
 // FILEPATH: components/Footer.js
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Footer = () => {
@@ -8,12 +9,19 @@ const Footer = () => {
     <>
       <footer className='text-center container mx-auto px-4'>
         <div className='m-4 p-4'>
-        <p>&copy; 2024 SuiPian. All rights reserved.</p>
-        <Link href='https://www.suiyan.cc' className='text-gray-300 transition hover:text-orange-500'>
-          <p>Powered by 碎言</p>
-        </Link>
+          <p>&copy; 2024 SuiPian. All rights reserved.</p>
+          <Link href='https://www.suiyan.cc' className='text-gray-300 transition hover:text-orange-500'>
+            <p>Powered by 碎言</p>
+          </Link>
+          <div className='flex justify-center'>
+            <Image
+              src={`/images/gzh.png`}
+              className="rounded-lg"
+              width="100" height="100" // Corrected to a numeric value in pixels
+            />
+          </div>
         </div>
-        
+
       </footer>
     </>
   );
