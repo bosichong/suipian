fa6f941d2cc0c3748f908a259cbf7927290d127c
J.sky <bosichong@qq.com>
2024-10-17 08:43:58 +0800

    更新统计代码

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ import Footer from '@/components/footer';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js?id=3FsLlPZR8s5Rn9BW&ck=3FsLlPZR8s5Rn9BW"></script>
+      </Head>
       <body className="antialiased">
         <SiteNav />
         <Main />
