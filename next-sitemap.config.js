2673afbb737cd48c2bb3406ea09b2f3789d85acc
J.sky <bosichong@qq.com>
2024-12-22 21:19:41 +0800

    更新

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.suipian.cc', // 替换为你的网站URL
+    generateRobotsTxt: true, // 是否生成robots.txt
+    sitemapSize: 7000, // 每个sitemap文件的URL数量
+    // 其他sitemap配置...
+  };
+  
